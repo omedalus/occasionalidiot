57c909d8dea23f3cd726a656345294c488bd4ade
Omedalus <mvol@live.com>
2015-02-18 01:04:26 -0500

    Chrome: preparing for context menus, but they need a background.js

--- chrome/src/occasionalidiot-facebook.js
+++ chrome/src/occasionalidiot-facebook.js
@@ -1,8 +1,20 @@
+// Occasional Idiot
+// Source file for Chrome plugin to handle Facebook integration.
+// Copyright (c) 2015 Mikhail Voloshin. All rights reserved.
 
 (function() {
 
 var FB_POST_DIV_CLASSNAME = '_5jmm';
 var FB_BLOCKER_ANCHOR_CLASSNAME = '_52c6';
+
+var APP_POST_DIV_CLASSNAME = 'occasionalidiot-marked-post';
+var APP_POST_USER_ATTRNAME = 'occasionalidiot-poster-username';
+
+var APP_CONTEXT_MENU_ID = 'occasionalidiot-context-menu';
+var APP_CONTEXT_MENU_TITLE = 'Occasional Idiot'; // LOCAL
+
+var currentPoster = null;
+var currentCommenter = null;
 
 // As every node is added to the DOM, check it to see if it's a wall post.
 var processWallPost = function(postNode)
@@ -35,12 +47,52 @@ var processWallPost = function(postNode)
     }
   }
 
-  // The user's full name is buried in an h5 element.
+  // The poster's full name is buried in an h5 element.
   // We'll use it as the key.
   var username = postNode.getElementsByTagName('h5')[0].
       getElementsByTagName('a')[0].
       innerText.trim();
-  console.log(username);
+      
+  // Annotate this element so our lookup can be much faster next time.
+  postNode.classList.add(APP_POST_DIV_CLASSNAME);
+  postNode.setAttribute(APP_POST_USER_ATTRNAME, username);
+  
+  postNode.addEventListener('mousedown', function(event) {
+    if (event.button === 2) {
+      // Right click.
+      currentCommenter = username;
+      console.log(currentCommenter);
+    }
+  });
+  
+  // Look up the blacklist collection.
+  chrome.storage.sync.get('blacklist', function(blacklist) {
+    // FAKE BLACKLIST UNTIL SAVING IS IMPLEMENTED
+    blacklist = {};
+    blacklist["Chicks On The Right"] = {words: ['DESPICABLE', 'insane']};
+    // End fake blacklist.
+  
+    if (!blacklist || !blacklist[username]) {
+      // This poster doesn't have a blacklist.
+      return;
+    }
+    
+    // Gather up all the post's text and comments 
+    // (or at least currently available comments).
+    var allPostText = postNode.innerText.toUpperCase();
+    var userBlacklistWords = blacklist[username].words;
+    
+    for (var iBlacklistWord = 0; 
+        iBlacklistWord < userBlacklistWords.length; 
+        iBlacklistWord++) {
+      var blacklistWord = userBlacklistWords[iBlacklistWord].toUpperCase();
+      if (allPostText.indexOf(blacklistWord) != -1) {
+        // The post or one of its comments contains a word that's been
+        // blacklisted for the original poster. Kill the whole post.
+        postNode.style.display = 'none';
+      }
+    }
+  });
 };
 
 document.addEventListener('DOMNodeInserted', function(event) {
@@ -52,14 +104,23 @@ document.addEventListener('DOMNodeInserted', function(event) {
     } else {
       var postNodes = node.getElementsByClassName(FB_POST_DIV_CLASSNAME);
       for (var iPostNode = 0; iPostNode < postNodes.length; iPostNode++) {
-        processWallPost(postNodes[iPostNode]);
+        var postNode = postNodes[iPostNode];
+        if (!postNode) {
+          continue;
+        }
+        
+        processWallPost(postNode);
       }
-    }
+    }    
   } catch (ex) {
-      console.log(ex);
     return;
   }
 });
+
+// Set up the context menus.
+console.log('hiiii');
+debugger;
+chrome.contextMenus.create({"title": "Test parent item"});
 
 })();
 
