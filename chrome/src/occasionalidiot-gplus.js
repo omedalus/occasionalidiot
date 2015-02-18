2e9c733b45c7428033541c4829b2ad5d1b28d9f8
Mikhail Voloshin <mvol@live.com>
2015-02-18 18:09:21 -0500

    Chrome: G+ notes

---
+++ chrome/src/occasionalidiot-gplus.js
@@ -0,0 +1,11 @@
+// Occasional Idiot
+// Source file for Chrome plugin to handle G+ integration.
+// G+ DOM manipulation routines.
+// Copyright (c) 2015 Mikhail Voloshin. All rights reserved.
+
+(function() {
+
+// Comment class: .Jx
+// Post class: .Ee
+
+})();
